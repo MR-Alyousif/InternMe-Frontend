70cd95fb38fa15d4816f29dc8de24be055ee2341
MR-Alyousif <mryousif967@gmail.com>
2024-04-19 14:24:01 +0300

    update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,ts,css}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#010517',
+        'primary': '#CE8459',
+        'secondary': {
+          100: '#242044',
+          200: '#F1C6B9',
+        }
+      },
+    },
   },
   plugins: [],
 }
