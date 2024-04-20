dd9bf6a67b386993d32c8cbe53d68ab7d50206a1
MR-Alyousif <mryousif967@gmail.com>
2024-04-20 13:57:19 +0300

    opportunity binding

--- src/app/pages/opportunity/opportunity.component.ts
+++ src/app/pages/opportunity/opportunity.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from '../../global-components/navbar/navbar.component';
 import { ButtonComponent } from '../../global-components/app-button/app-button.component';
 
 @Component({
   selector: 'app-opportunity',
   standalone: true,
-  imports: [NavbarComponent, ButtonComponent],
+  imports: [FormsModule, NavbarComponent, ButtonComponent],
   templateUrl: './opportunity.component.html',
   styleUrl: './opportunity.component.css'
 })
 export class OpportunityComponent {
+  opportunityData: any = {
+    name: '',
+    major: '',
+    description: '',
+    startingDate: '',
+    duration: '',
+    location: '',
+    url: '',
+    responsibilities: '',
+    qualifications: '',
+    requiredGPA4: '',
+    requiredGPA5: '',
+    skills: ''
+  };
+  
+  onSubmit() {
+    console.log('Submitted');
+  }
 
 }
