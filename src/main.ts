34a0463eb01da14f6c0bff40f252cde1f0189ffc
Hadi Alqattan <alqattanhadizaki@gmail.com>
2024-04-20 17:31:54 +0300

    fix: missing app entrypoint

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { HomepageComponent } from './app/pages/homepage/homepage.component';
+
+bootstrapApplication(HomepageComponent, appConfig).catch((err) =>
+  console.error(err),
+);
