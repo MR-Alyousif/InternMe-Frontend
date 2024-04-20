1d4092460cb18439c1c5a459bda260c6df873bfc
Hadi Alqattan <alqattanhadizaki@gmail.com>
2024-04-20 21:12:16 +0300

    add: homepage (todo: resp)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts,css}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{html,ts,css}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        'background': '#010517',
-        'primary': '#242044',
-        'secondary': {
-          100: '#CE8459',
-          200: '#F1C6B9',
+        background: "#010517",
+        primary: "#242044",
+        secondary: {
+          100: "#CE8459",
+          150: "#FB7E65",
+          200: "#F1C6B9",
         },
-        'green': '#CCFF90',
-        'dark-blue': '#190692',
-        'gradient': {
-          100: 'linear-gradient(to right,#110561, #FB7E65)'
-        }
-      }
+        green: "#CCFF90",
+        "dark-blue": "#190692",
+        gradient: {
+          100: "linear-gradient(to right,#110561, #FB7E65)",
+        },
+      },
     },
   },
   corePlugins: {
     aspectRatio: false,
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('tailwind-scrollbar')({ preferredStrategy: 'pseudoelements' }),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwind-scrollbar")({ preferredStrategy: "pseudoelements" }),
   ],
-}
-
+};
