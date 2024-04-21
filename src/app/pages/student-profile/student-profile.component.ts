30e6688700c97971a22a1890e11d010e7bdfbd64
MR-Alyousif <mryousif967@gmail.com>
2024-04-21 10:29:37 +0300

    update student profile

--- src/app/pages/student-profile/student-profile.component.ts
+++ src/app/pages/student-profile/student-profile.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student-profile',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './student-profile.component.html',
   styleUrl: './student-profile.component.css',
 })
-export class StudentProfileComponent {}
+export class StudentProfileComponent {
+  info: any = {
+    fullName: 'Khalid Ahmed',
+    major: 'Software Engineering',
+    description: "Aspiring front-end developer with a passion for crafting engaging and user-eccentric web experiences. I leverage strong coding skills and a creative eye to translate complex functionalities into intuitive interfaces. Always eager to learn and collaborate, I'm actively seeking opportunities to contribute to innovative front-end projects.",
+    email: 'khalid_ahmed@gmail.com',
+    phone: '05000020394',
+    university: 'King Fahd University of Petroleum and Minerals',
+    projectsDescription: "Aspiring front-end developer with a passion for crafting engaging and user-eccentric web experiences. I leverage strong coding skills and a creative eye to translate complex functionalities into intuitive interfaces. Always eager to learn and collaborate, I'm actively seeking opportunities to contribute to innovative front-end projects.",
+    skills: ['UI Designer', 'Figma', 'Landing Page']
+  };
+}
