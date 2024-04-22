d3b916c2eb0aa57071214ff5d44c74c90bd1a6df
MR-Alyousif <mryousif967@gmail.com>
2024-04-22 17:33:47 +0300

    Update company profile

--- src/app/pages/company-profile/company-profile.component.ts
+++ src/app/pages/company-profile/company-profile.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../global-components/navbar/navbar.component';
-import { ButtonComponent } from '../../global-components/app-button/app-button.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-company-profile',
   standalone: true,
-  imports: [NavbarComponent, ButtonComponent],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './company-profile.component.html',
-  styleUrl: './company-profile.component.css'
+  styleUrl: './company-profile.component.css',
 })
 export class CompanyProfileComponent {
+  companyInfo: any = {
+    name: 'Microsoft',
+    website: 'www.microsoft.com',
+    location: 'Riyadh',
+    email: 'microsoft@mix.org',
+    phone: '05000020394',
+  };
 
+  opportunities: any[] = [
+    {
+      title: 'Full Stack Developer',
+      description:
+        'We are looking for full stack developer who can help develop the entire mobile application ... As a Microsoft Full Stack Developer, you will play a pivotal role in designing, developing, and maintaining software applications using Microsoft technologies.',
+      skills: ['UI Designer', 'Figma', 'Landing Page'],
+      duration: '8 weeks',
+      date: '2024/5/6',
+      location: 'Dhahran',
+    },
+  ];
 }
