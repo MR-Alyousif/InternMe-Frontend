6f93fbf517e8245b65aa7e2bd1e762f4ba4405fd
Hadi Alqattan <alqattanhadizaki@gmail.com>
2024-04-22 18:33:14 +0300

    fix: trainin-details navbar

--- src/app/pages/trainin-details/trainin-details.component.ts
+++ src/app/pages/trainin-details/trainin-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-trainin-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './trainin-details.component.html',
   styleUrl: './trainin-details.component.css',
 })
