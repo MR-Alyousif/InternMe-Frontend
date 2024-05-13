3217d8c8dbf4ecc78443db1b29dc6f9541aeaef5
MR-Alyousif <mryousif967@gmail.com>
2024-05-13 13:02:28 +0300

    Formatted code with Prettier.

--- src/app/pages/authentication/sign-in/sign-in.component.ts
+++ src/app/pages/authentication/sign-in/sign-in.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
@@ -13,46 +13,59 @@ export class SignInComponent {
   username: string = '';
   password: string = '';
   emailForRecovery: string = ''; // Email input for password recovery
-  showModal: boolean = false; 
+  showModal: boolean = false;
 
   constructor(private router: Router) {}
 
-
-  async signIn() { // Mark the function as 
-    const usernameInput = document.getElementById('usernameLogin') as HTMLInputElement;
-    const passwordInput = document.getElementById('passwordLogin') as HTMLInputElement;
+  async signIn() {
+    // Mark the function as
+    const usernameInput = document.getElementById(
+      'usernameLogin',
+    ) as HTMLInputElement;
+    const passwordInput = document.getElementById(
+      'passwordLogin',
+    ) as HTMLInputElement;
     if (!usernameInput.value.trim() || !passwordInput.value.trim()) {
       alert('Please enter both Username and Password.');
-      
+
       // Make the border of empty fields red
       if (!usernameInput.value.trim()) {
         usernameInput.style.borderColor = 'red';
-        usernameInput.insertAdjacentHTML('afterend', '<span style="color: red">required</span><i class="fas fa-exclamation-circle" style="color: red"></i>');      }
+        usernameInput.insertAdjacentHTML(
+          'afterend',
+          '<span style="color: red">required</span><i class="fas fa-exclamation-circle" style="color: red"></i>',
+        );
+      }
       if (!passwordInput.value.trim()) {
         passwordInput.style.borderColor = 'red';
-        passwordInput.insertAdjacentHTML('afterend', '<span style="color: red">required</span><i class="fas fa-exclamation-circle" style="color: red"></i>');
+        passwordInput.insertAdjacentHTML(
+          'afterend',
+          '<span style="color: red">required</span><i class="fas fa-exclamation-circle" style="color: red"></i>',
+        );
       }
-      
+
       return; // Exit function if fields are empty
     }
-    
+
     this.username = usernameInput.value;
     this.password = passwordInput.value;
 
     console.log('Signing in with username:', this.username);
 
     try {
-      const response = await fetch('https://intern-me.ddns.net/api/v1/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
+      const response = await fetch(
+        'https://intern-me.ddns.net/api/v1/users/login',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password,
+          }),
         },
-        body: JSON.stringify({
-          username: this.username,
-          password: this.password
-        })
-      });
-
+      );
 
       if (!response.ok) {
         throw new Error('Login failed');
@@ -60,26 +73,20 @@ export class SignInComponent {
 
       const data = await response.json();
       console.log('Login successful. Token:', data.token);
-      // Handle success, the token in local storage to later use 
+      // Handle success, the token in local storage to later use
       localStorage.setItem('token', data.token);
-      this.router.navigate(['/'])
-
+      this.router.navigate(['/']);
     } catch (error) {
       console.error('Login failed. Error:', error);
       // Handle error
     }
   }
-  
 
-
-  dontHaveAc(){
-    this.router.navigate(['/sign-up'])
-
+  dontHaveAc() {
+    this.router.navigate(['/sign-up']);
   }
 
-
-
-  // password forgot logic  
+  // password forgot logic
   forgotPassword() {
     console.log('Forgot password with username:', this.username);
     // Display the password recovery modal
@@ -103,7 +110,9 @@ export class SignInComponent {
     console.log('Sending password recovery email to:', this.emailForRecovery);
 
     // Show message to the user
-    alert('Your username and password have been sent to ' + this.emailForRecovery);
+    alert(
+      'Your username and password have been sent to ' + this.emailForRecovery,
+    );
 
     // Close the password recovery modal after sending the email
     this.closeModal();
--- src/app/pages/authentication/sign-up/sign-up.component.ts
+++ src/app/pages/authentication/sign-up/sign-up.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
@@ -18,30 +18,51 @@ export class SignUpComponent {
   constructor(private router: Router) {}
 
   async signUp() {
-    const emailInputReg = (document.getElementById('emailReg') as HTMLInputElement);
-    const usernameInputReg = (document.getElementById('usernameReg') as HTMLInputElement);
-    const passwordInputReg = (document.getElementById('passwordReg') as HTMLInputElement);
-    const ruleInputReg = (document.getElementById("rule") as HTMLSelectElement).value;
+    const emailInputReg = document.getElementById(
+      'emailReg',
+    ) as HTMLInputElement;
+    const usernameInputReg = document.getElementById(
+      'usernameReg',
+    ) as HTMLInputElement;
+    const passwordInputReg = document.getElementById(
+      'passwordReg',
+    ) as HTMLInputElement;
+    const ruleInputReg = (document.getElementById('rule') as HTMLSelectElement)
+      .value;
 
-
-    if (!usernameInputReg.value.trim() || !passwordInputReg.value.trim() || !emailInputReg.value.trim()) {
+    if (
+      !usernameInputReg.value.trim() ||
+      !passwordInputReg.value.trim() ||
+      !emailInputReg.value.trim()
+    ) {
       alert('Please enter all required fields');
-      
+
       // Make the border of empty fields red
       if (!usernameInputReg.value.trim()) {
         usernameInputReg.style.borderColor = 'red';
-        usernameInputReg.insertAdjacentHTML('afterend', '<span style="color: red">required</span><i class="fas fa-exclamation-circle" style="color: red"></i>');      
+        usernameInputReg.insertAdjacentHTML(
+          'afterend',
+          '<span style="color: red">required</span><i class="fas fa-exclamation-circle" style="color: red"></i>',
+        );
       }
-      if (!passwordInputReg.value.trim() || passwordInputReg.value.length <8) {
+      if (!passwordInputReg.value.trim() || passwordInputReg.value.length < 8) {
         passwordInputReg.style.borderColor = 'red';
-        passwordInputReg.insertAdjacentHTML('afterend', '<span style="color: red">required & Password must be at least 8 characters long.</span><i class="fas fa-exclamation-circle" style="color: red"></i>');
+        passwordInputReg.insertAdjacentHTML(
+          'afterend',
+          '<span style="color: red">required & Password must be at least 8 characters long.</span><i class="fas fa-exclamation-circle" style="color: red"></i>',
+        );
       }
-      if (!emailInputReg.value.trim() || !this.validateEmail(emailInputReg.value)) {
+      if (
+        !emailInputReg.value.trim() ||
+        !this.validateEmail(emailInputReg.value)
+      ) {
         emailInputReg.style.borderColor = 'red';
-        emailInputReg.insertAdjacentHTML('afterend', '<span style="color: red">required & Please enter a valid email address.</span><i class="fas fa-exclamation-circle" style="color: red"></i>');
+        emailInputReg.insertAdjacentHTML(
+          'afterend',
+          '<span style="color: red">required & Please enter a valid email address.</span><i class="fas fa-exclamation-circle" style="color: red"></i>',
+        );
       }
-      
-      
+
       return; // Exit function if fields are empty
     }
 
@@ -50,33 +71,34 @@ export class SignUpComponent {
     this.email = emailInputReg.value;
     this.rule = ruleInputReg;
 
-    console.log("username:" + this.username)
-    console.log(this.password)
-    console.log(this.email)
-    console.log(this.rule)
-
-
+    console.log('username:' + this.username);
+    console.log(this.password);
+    console.log(this.email);
+    console.log(this.rule);
 
     try {
-      const response = await fetch('https://intern-me.ddns.net/api/v1/users/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
+      const response = await fetch(
+        'https://intern-me.ddns.net/api/v1/users/register',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: this.email,
+            username: this.username,
+            password: this.password,
+            role: this.rule,
+          }),
         },
-        body: JSON.stringify({
-          email: this.email,
-          username: this.username,
-          password: this.password,
-          role: this.rule
-        })
-      });
-  
+      );
+
       if (!response.ok) {
         throw new Error('Sign up failed');
       }
-  
+
       const data = await response.json();
-      this.login(this.username,this.password);
+      this.login(this.username, this.password);
 
       // Optionally handle successful sign up response
     } catch (error) {
@@ -85,57 +107,57 @@ export class SignUpComponent {
     }
   }
 
-  HaveAccount(){
-    this.router.navigate(['/sign-in'])
-
+  HaveAccount() {
+    this.router.navigate(['/sign-in']);
   }
 
-    // Email validation function
-    validateEmail(email: string): boolean {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailRegex.test(email);
-    }
-    // remove error message 
-    clearErrorAndStyling(inputElement: HTMLInputElement) {
-      // Reset border color
-      inputElement.style.borderColor = '';
-    
-      // Remove error message span and icon if they exist
-      const errorMessageElement = inputElement.nextElementSibling;
-      if (errorMessageElement) {
-        errorMessageElement.remove();
-      }
-    }
+  // Email validation function
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  // remove error message
+  clearErrorAndStyling(inputElement: HTMLInputElement) {
+    // Reset border color
+    inputElement.style.borderColor = '';
 
+    // Remove error message span and icon if they exist
+    const errorMessageElement = inputElement.nextElementSibling;
+    if (errorMessageElement) {
+      errorMessageElement.remove();
+    }
+  }
 
-    //auto login after successful register 
-    async login(username: string, password: string) {
-      try {
-        const response = await fetch('https://intern-me.ddns.net/api/v1/users/login', {
+  //auto login after successful register
+  async login(username: string, password: string) {
+    try {
+      const response = await fetch(
+        'https://intern-me.ddns.net/api/v1/users/login',
+        {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             username: username,
-            password: password
-          })
-        });
-    
-        if (!response.ok) {
-          throw new Error('Login failed');
-        }
-    
-        const data = await response.json();
-        // Handle success, store the token in local storage for later use
-        alert('Registration successful!');
-        localStorage.setItem('token', data.token);
-        this.router.navigate(['/']);
-      } catch (error) {
-        console.error('Login failed. Error:', error);
-        // Handle error
-        alert('Login failed. Please check your username and password.');
-      }
-    }
-}
+            password: password,
+          }),
+        },
+      );
 
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const data = await response.json();
+      // Handle success, store the token in local storage for later use
+      alert('Registration successful!');
+      localStorage.setItem('token', data.token);
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.error('Login failed. Error:', error);
+      // Handle error
+      alert('Login failed. Please check your username and password.');
+    }
+  }
+}
