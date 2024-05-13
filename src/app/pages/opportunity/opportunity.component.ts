0d88a12f3f1e4109096ac020151430f93153bb17
MR-Alyousif <mryousif967@gmail.com>
2024-05-13 17:35:21 +0300

    Added 'opportunity' endpoint.

--- src/app/pages/opportunity/opportunity.component.ts
+++ src/app/pages/opportunity/opportunity.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { NavbarComponent } from '../../global-components/navbar/navbar.component';
 import { ButtonComponent } from '../../global-components/app-button/app-button.component';
@@ -19,6 +20,8 @@ import { ButtonComponent } from '../../global-components/app-button/app-button.c
   styleUrl: './opportunity.component.css',
 })
 export class OpportunityComponent {
+  baseUrl: string = 'https://intern-me.ddns.net/api/v1';
+  token: string | null = localStorage.getItem('token');
   opportunityData: any = {
     name: '',
     major: '',
@@ -34,7 +37,43 @@ export class OpportunityComponent {
     skills: '',
   };
 
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
   onSubmit() {
-    console.log('Submitted');
+    const url = `${this.baseUrl}/offers/create`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-auth': `Bearer ${this.token}`,
+    });
+
+    this.http.post<any>(url, this.opportunityData, { headers }).subscribe({
+      next: (response) => {
+        this.successMessage();
+        this.opportunityData = {
+          name: '',
+          major: '',
+          description: '',
+          startingDate: '',
+          duration: '',
+          location: '',
+          url: '',
+          responsibilities: '',
+          qualifications: '',
+          requiredGPA4: '',
+          requiredGPA5: '',
+          skills: '',
+        };
+        this.router.navigate(['/company-profile']);
+      },
+      error: (error) => {
+        console.error('Error adding opportunity:', error);
+      },
+    });
+  }
+  private successMessage() {
+    alert('Opportunity added successfully');
   }
 }
