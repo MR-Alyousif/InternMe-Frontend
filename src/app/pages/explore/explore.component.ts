97bcbaf933aeef584b7bd0abefbb1155239dd299
Hadi Alqattan <alqattanhadizaki@gmail.com>
2024-05-15 13:21:07 +0300

    fix: make explore work with pagination

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -22,95 +22,69 @@ import { NavbarComponent } from '../../global-components/navbar/navbar.component
 export class ExploreComponent {
   baseUrl: string = 'https://intern-me.ddns.net/api/v1';
   token: string | null = localStorage.getItem('token');
+
+  opportunities: any[] = [];
   searchQuery: string = '';
-  cards: any[] = [];
-  currentPage: number = 1;
-  cardsPerPage: number = 4;
+  pageNumber: number = 1;
+  opportunitiesPerPage: number = 4;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    // Initialize cards for testing
-    this.initializeCards();
+    this.fetchOpportunities();
   }
 
-  fetchStudentProfile() {
-    const url = `${this.baseUrl}/offers/list?page=${this.currentPage}&limit=${this.cardsPerPage}`;
+  fetchOpportunities() {
+    const url = `${this.baseUrl}/offers/list`;
+    const logoUrl = `${this.baseUrl}/profiles/static`;
 
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'x-auth': `${this.token}`,
     });
 
-    this.http.get<any>(url, { headers }).subscribe({
+    let urlWithSearchQuery = `${url}?`;
+    urlWithSearchQuery += `page=${this.pageNumber}`;
+    urlWithSearchQuery += `&limit=${this.opportunitiesPerPage}`;
+
+    this.http.get<any>(urlWithSearchQuery, { headers }).subscribe({
       next: (response) => {
-        this.cards = response.offers
+        if (response.offers) {
+          this.opportunities = response.offers.map((offer: any) => {
+            const date = new Date(offer.startingDate);
+            return {
+              _id: offer._id,
+              name: offer.name,
+              description: offer.description,
+              startingDate: `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`,
+              duration: `${offer.durationInWeeks / 4} months`,
+              location: offer.location,
+              skills: offer.skills,
+              company: {
+                name: offer.company.name,
+                logo: `${logoUrl}/${offer.company.logo}`,
+              },
+            };
+          });
+        } else {
+          this.opportunities = [];
+        }
       },
-      
+      error: (error) => {
+        console.error('Error fetching opportunities:', error);
+      },
     });
   }
 
-  initializeCards() {
-    // Sample card data for testing
-    this.cards = [
-      {
-        logo: 'assets/logo/aramco.png',
-        company: 'Aramco',
-        title: 'Mechanical Engineering',
-        duration: '8 weeks',
-        date: '2024/5/6',
-        location: 'Dhahran',
-        description:
-          'We are looking for Mechanical engineering who can help design with Solidwork...',
-        skills: ['UI Designer', 'Figma', 'Landing Page'],
-      },
-
-      {
-        logo: 'assets/logo/googleLogo.png',
-        company: 'Google',
-        title: 'Software Development',
-        duration: '10 weeks',
-        date: '2024/6/15',
-        location: 'New York',
-        description:
-          'Looking for software developers proficient in JavaScript, Angular, and Node.js.',
-        skills: ['JavaScript', 'Angular', 'Node.js'],
-      },
-      {
-        logo: 'assets/logo/googleLogo.png',
-        company: 'Google',
-        title: 'Data Science Internship',
-        duration: '12 weeks',
-        date: '2024/7/20',
-        location: 'San Francisco',
-        description:
-          'Opportunity for data science enthusiasts to work on real-world projects.',
-        skills: ['Python', 'Machine Learning', 'Data Analysis'],
-      },
-      {
-        logo: 'assets/logo/amazon.png',
-        company: 'Amazon',
-        title: 'Marketing Intern',
-        duration: '6 weeks',
-        date: '2024/8/10',
-        location: 'London',
-        description:
-          'Seeking creative individuals to assist with marketing campaigns and social media management.',
-        skills: ['Social Media Marketing', 'Content Creation', 'SEO'],
-      },
-    ];
-  }
-
   previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
+    if (this.pageNumber > 1) {
+      this.pageNumber--;
     }
   }
 
   nextPage() {
-    const totalPages = Math.ceil(this.cards.length / this.cardsPerPage);
-    if (this.currentPage < totalPages) {
-      this.currentPage++;
+    if (this.opportunities.length >= this.opportunitiesPerPage) {
+      this.pageNumber++;
     }
   }
 }
