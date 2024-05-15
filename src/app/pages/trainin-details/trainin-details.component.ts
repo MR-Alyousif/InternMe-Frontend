72d49ab7c0ad071e9dcff4be31c1ae1ee21ea097
Hadi Alqattan <alqattanhadizaki@gmail.com>
2024-05-15 15:13:09 +0300

    fix: opportunity details page

--- src/app/pages/trainin-details/trainin-details.component.ts
+++ src/app/pages/trainin-details/trainin-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NavbarComponent } from '../../global-components/navbar/navbar.component';
 
@@ -14,29 +14,61 @@ import { NavbarComponent } from '../../global-components/navbar/navbar.component
 export class TraininDetailsComponent {
   baseUrl: string = 'https://intern-me.ddns.net/api/v1';
   token: string | null = localStorage.getItem('token');
-  offerId: string | null = localStorage.getItem('offerId');
+  offerId: string | undefined;
 
-  training: any = {};
+  opportunity = {
+    description:
+      '##############################################################',
+    responsibilities: [
+      '##############################',
+      '##############################',
+      '##############################',
+    ],
+    qualifications: [
+      '##############################',
+      '##############################',
+      '##############################',
+    ],
+    company: {
+      name: '#########',
+      logo: '../../../assets/profile/default_logo.jpg',
+    },
+  };
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+  ) {}
+
   ngOnInit(): void {
-    this.fetchTrainingDetails();
+    this.route.params.subscribe((params) => (this.offerId = params['offerId']));
+    this.fetchOpportunityDetails();
   }
 
-  fetchTrainingDetails() {
+  fetchOpportunityDetails() {
     const url = `${this.baseUrl}/offers/${this.offerId}`;
+    const logoUrl = `${this.baseUrl}/profiles/static`;
 
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization: `${this.token}`,
+      'x-auth': `${this.token}`,
     });
 
     this.http.get<any>(url, { headers }).subscribe({
-      next: (response) => {
-        this.training = response.offer;
+      next: (res) => {
+        this.opportunity = {
+          // i know that this has redundency; but i wanted to show data field names ;-)
+          description: res.offer.description,
+          responsibilities: res.offer.responsibilities,
+          qualifications: res.offer.qualifications,
+          company: {
+            name: res.offer.company.name,
+            logo: `${logoUrl}/${res.offer.company.logo}`,
+          },
+        };
       },
       error: (error) => {
-        console.error('Error fetching training details', error);
+        console.error('Error fetching opportunity details', error);
       },
     });
   }
